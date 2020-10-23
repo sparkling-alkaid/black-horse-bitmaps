64e5f40c0cfeca3e320fa575cee3f0c646cd6cc9
Johonzoson <Johonzoson@outlook.com>
2020-10-23 20:29:48 +0800

    生成bitmap组相关代码

--- src/main/java/com/glab/black/horse/bitmap/service/BitmapMakeService.java
+++ src/main/java/com/glab/black/horse/bitmap/service/BitmapMakeService.java
@@ -2,17 +2,25 @@ package com.glab.black.horse.bitmap.service;
 
 
 import com.glab.black.horse.bitmap.pojo.entity.TagMeta;
+import com.glab.black.horse.bitmap.pojo.model.NumberBitmapGroup;
+import com.glab.black.horse.bitmap.pojo.model.Tag2RoaringBitmap;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BitmapMakeService {
 
+
+    @Autowired
+    private BitmapImportService bitmapImportService;
 
     String dataFile = "";
 
@@ -21,7 +29,14 @@ public class BitmapMakeService {
 
         File dataFile = new File(filePath);
 
-        Files.lines(dataFile.toPath()).map(line -> {
+        List<NumberBitmapGroup> numberBitmapGroups = new ArrayList<NumberBitmapGroup>();
+
+        metaList.forEach(tagMeta -> {
+            numberBitmapGroups.add(new NumberBitmapGroup(tagMeta.getTagId()));
+        });
+
+
+        Files.lines(dataFile.toPath()).forEach(line -> {
             //0,3420,1010,2263.61,5663.63
             //1,2120,441,8334.31,466.74
             //2,1436,3475,4418.21,2474.56
@@ -30,24 +45,25 @@ public class BitmapMakeService {
             //5,183,8496,8676.36,5515.76
             //6,1483,5666,4927.33,7573.11
 
+
             if ("".equals(line)) {
-                return null;
+                return;
             }
             String[] strings = line.split(",", -1);
-            if (strings.length != 6) {
-                return null;
+            if (strings.length != metaList.size() + 1) {
+                return;
             }
-            int offset = 0;
-            int id = Integer.parseInt(strings[offset++]);
-            int pro1 = Integer.parseInt(strings[offset++]);
-            int pro2 = Integer.parseInt(strings[offset++]);
-            BigDecimal pro3 = new BigDecimal(strings[offset++]);
-            BigDecimal pro4 = new BigDecimal(strings[offset++]);
 
+            int id = Integer.parseInt(strings[0]);
 
-            return null;
+            for (int i = 0; i < metaList.size(); i++) {
+                int tagValue = new BigDecimal(strings[i + 1]).multiply(new BigDecimal(10).pow(metaList.get(i).getPrecision())).intValue();
+                numberBitmapGroups.get(i).addNumber(id, tagValue);
+            }
         });
+        List<Tag2RoaringBitmap> list = numberBitmapGroups.stream().flatMap(numberBitmapGroup -> numberBitmapGroup.toBitmapList().stream()).collect(Collectors.toList());
 
+        bitmapImportService.importTag2Bitmap(list);
 
     }
 
