d04b9f9a239dd39bb3768dc311becc18f0860ed0
funnyhood <funnyhood@163.com>
2020-10-23 11:46:03 +0800

    empty project

---
+++ src/main/java/com/glab/black/horse/bitmap/BlackHorseApplication.java
@@ -0,0 +1,13 @@
+package com.glab.black.horse.bitmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackHorseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlackHorseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glab/black/horse/bitmap/BlackHorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glab.black.horse.bitmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackHorseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
