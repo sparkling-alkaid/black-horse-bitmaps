ac506719a74284e7cd2d120e6a270693c06ad834
fangxin <fangx1129@outlook.com>
2020-10-23 15:48:19 +0800

    add controller

---
+++ src/main/java/com/glab/black/horse/bitmap/controller/QueryController.java
@@ -0,0 +1,24 @@
+package com.glab.black.horse.bitmap.controller;
+
+import com.glab.black.horse.bitmap.entity.QueryReq;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QueryController {
+
+    /**
+     * {"action":"query","rule":"prop1<10 && prop2<=20 && prop3>30.5 && prop4>=40.9"}
+     *
+     * @param
+     * @return
+     */
+    @RequestMapping("/query")
+    public String home(QueryReq req) {
+        String rule = req.getRule();
+        
+        return "Hello World!";
+
+    }
+
+}
---
+++ src/main/java/com/glab/black/horse/bitmap/entity/QueryReq.java
@@ -0,0 +1,15 @@
+package com.glab.black.horse.bitmap.entity;
+
+import lombok.Data;
+
+/**
+ *
+ */
+@Data
+public class QueryReq {
+
+    private String action;
+
+    private String rule;
+
+}
