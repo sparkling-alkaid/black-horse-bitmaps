f8eb9598661d49af5a27fc6b70d12ba0a2bcb66e
funnyhood <funnyhood@163.com>
2020-10-23 16:13:40 +0800

    addnumber

--- src/main/java/com/glab/black/horse/bitmap/pojo/model/NumberBitmapGroup.java
+++ src/main/java/com/glab/black/horse/bitmap/pojo/model/NumberBitmapGroup.java
@@ -2,13 +2,13 @@ package com.glab.black.horse.bitmap.pojo.model;
 
 
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import lombok.Data;
 import org.roaringbitmap.RoaringBitmap;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Data
 public class NumberBitmapGroup {
@@ -25,6 +25,7 @@ public class NumberBitmapGroup {
     }
 
     public NumberBitmapGroup(String name, int size) {
+        this.name = name;
         notNullBitmap = new RoaringBitmap();
         bitmapMap = new HashMap<>((int) ((size + 1) / 0.75) + 1);
         for (int i = 0; i < size; i++) {
@@ -32,42 +33,63 @@ public class NumberBitmapGroup {
         }
     }
 
+    public NumberBitmapGroup(String name, RoaringBitmap notNullBitmap, Map<Integer, RoaringBitmap> bitmapMap) {
+        this.name = name;
+        this.notNullBitmap = notNullBitmap;
+        this.bitmapMap = bitmapMap;
+    }
+
     public RoaringBitmap getBitmapAtIndex(int index) {
-        return bitmapMap.get(index);
+        RoaringBitmap rb = bitmapMap.get(index);
+        return rb == null ? new RoaringBitmap() : rb;
     }
 
 
-    public void addNumber(int value) {
-        notNullBitmap.add(value);
+    public void addNumber(int id, int value) {
+        notNullBitmap.add(id);
+        char[] chars = intValToCharArr(value);
+        for (int index = 0; index < 32; index++) {
+            if (chars[index] == '0') {
+                getBitmapAtIndex(index).add(id);
+            }
+        }
+    }
+
+    private char[] intValToCharArr(int value) {
         String binary = Integer.toBinaryString(value);
         String fullBinary = Strings.padStart(binary, 32, '0');
-        char[] chars = fullBinary.toCharArray();
-
+        char[] chars = new char[fullBinary.length()];
+        for (int index = fullBinary.length() - 1; index >= 0; index--) {
+            chars[index] = fullBinary.charAt(index);
+        }
+        return chars;
     }
 
-    public RoaringBitmap gte(int value){
+    public RoaringBitmap gte(int value) {
         return null;
     }
 
-    public RoaringBitmap gt(int value){
+    public RoaringBitmap gt(int value) {
         return null;
     }
 
-    public RoaringBitmap lte(int value){
+    public RoaringBitmap lte(int value) {
+
         return null;
     }
 
-    public RoaringBitmap lt(int value){
+    public RoaringBitmap lt(int value) {
+        RoaringBitmap ret = new RoaringBitmap();
         return null;
     }
 
-    public RoaringBitmap eq(int value){
+    public RoaringBitmap eq(int value) {
         return null;
     }
 
 
-    public List<Tag2RoaringBitmap> toBitmapList(){
-        List<Tag2RoaringBitmap> list = new ArrayList<>(bitmapMap.size()+1);
+    public List<Tag2RoaringBitmap> toBitmapList() {
+        List<Tag2RoaringBitmap> list = new ArrayList<>(bitmapMap.size() + 1);
         list.add(new Tag2RoaringBitmap(name, -1, notNullBitmap));
         bitmapMap.forEach((k, v) -> list.add(new Tag2RoaringBitmap(name, k, v)));
         return list;
--- src/main/java/com/glab/black/horse/bitmap/service/BitmapImportService.java
+++ src/main/java/com/glab/black/horse/bitmap/service/BitmapImportService.java
@@ -5,6 +5,7 @@ import com.glab.black.horse.bitmap.pojo.entity.Tag2Bitmap;
 import com.glab.black.horse.bitmap.pojo.model.NumberBitmapGroup;
 import com.glab.black.horse.bitmap.pojo.model.Tag2RoaringBitmap;
 import com.glab.black.horse.bitmap.repo.Tag2BitmapRepo;
+import com.glab.black.horse.bitmap.utils.BitmapUtils;
 import org.roaringbitmap.RoaringBitmap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
@@ -24,27 +25,33 @@ public class BitmapImportService {
     private Tag2BitmapRepo tag2BitmapRepo;
 
 
-    public void importTag2Bitmap(List<Tag2RoaringBitmap> roaringBitmaps){
+    public void importTag2Bitmap(List<Tag2RoaringBitmap> roaringBitmaps) {
 
-        if(CollectionUtils.isEmpty(roaringBitmaps)){
+        if (CollectionUtils.isEmpty(roaringBitmaps)) {
             return;
         }
-        List<Tag2Bitmap> list = roaringBitmaps.stream().map( s -> new Tag2Bitmap(s) ).collect(Collectors.toList());
+        List<Tag2Bitmap> list = roaringBitmaps.stream().map(s -> new Tag2Bitmap(s)).collect(Collectors.toList());
 
         tag2BitmapRepo.saveAll(list);
 
     }
 
-    public NumberBitmapGroup getBitmapGroupById(String tagId){
+    public NumberBitmapGroup getBitmapGroupById(String tagId) {
         // 之后可以先获取meta，再返回合适的bitmap，现在单纯返回int的bitmap
         Tag2Bitmap example = new Tag2Bitmap();
         example.setTagId(tagId);
         List<Tag2Bitmap> tag2Bitmaps = tag2BitmapRepo.findAll(Example.of(example));
+        RoaringBitmap notNullBitmap = null;
         Map<Integer, RoaringBitmap> bitmapMap = new HashMap<>();
-        return null;
-
+        for (Tag2Bitmap tb : tag2Bitmaps) {
+            if (tb.getBitIndex() < 0) {
+                notNullBitmap = BitmapUtils.deserialize(tb.getBitmap());
+            } else {
+                bitmapMap.put(tb.getBitIndex(), BitmapUtils.deserialize(tb.getBitmap()));
+            }
+        }
+        return new NumberBitmapGroup(tagId, notNullBitmap, bitmapMap);
     }
-
 
 
 }
